13c5e4ef6bdbad2a5e9dffdaa5a33b16b1e731d1
SergeyGetmanskiy <getmansky1997@yandex.ru>
2024-11-14 23:10:28 +0300

    fix AdminFooter elements alignment

--- src/pages/Admin/AdminFooter/AdminFooter.tsx
+++ src/pages/Admin/AdminFooter/AdminFooter.tsx
@@ -26,31 +26,27 @@ function AdminFooter() {
     return (
         <footer className={styles.admin_footer}>
             <div className={`${styles.admin_footer__container}`}>
-                <Link to={isOrdersActive ? '/admin' : '/admin/orders'}>
-                    <div className={styles.admin_footer__element_container}>
-                        <button
-                            title={t('pages.admin.orders')}
-                            className={`
-                                ${styles.admin_footer__icon}
-                                ${styles.admin_footer__orders}
-                                ${isOrdersActive ? styles.admin_footer__orders_active : ''}
-                            `}
-                        />
-                        <h3 className={`${styles.admin_footer__element_title} ${isOrdersActive ? styles.admin_footer__element_title_active : ''}`}>{t('pages.admin.orders')}</h3>
-                    </div>
+                <Link to={isOrdersActive ? '/admin' : '/admin/orders'} className={styles.admin_footer__element_container}>
+                    <button
+                        title={t('pages.admin.orders')}
+                        className={`
+                            ${styles.admin_footer__icon}
+                            ${styles.admin_footer__orders}
+                            ${isOrdersActive ? styles.admin_footer__orders_active : ''}
+                        `}
+                    />
+                    <h3 className={`${styles.admin_footer__element_title} ${isOrdersActive ? styles.admin_footer__element_title_active : ''}`}>{t('pages.admin.orders')}</h3>
                 </Link>
-                <Link to={isWorkStatusActive ? '/admin' : '/admin/work-status'}>
-                    <div className={styles.admin_footer__element_container}>
-                        <button
-                            title={t('pages.admin.workStatus')}
-                            className={`
-                                ${styles.admin_footer__icon}
-                                ${styles.admin_footer__work_status}
-                                ${isWorkStatusActive ? styles.admin_footer__work_status_active : ''}
-                            `}
-                        />
-                        <h3 className={`${styles.admin_footer__element_title} ${isWorkStatusActive ? styles.admin_footer__element_title_active : ''}`}>{t('pages.admin.workStatus')}</h3>
-                    </div>
+                <Link to={isWorkStatusActive ? '/admin' : '/admin/work-status'} className={styles.admin_footer__element_container}>
+                    <button
+                        title={t('pages.admin.workStatus')}
+                        className={`
+                            ${styles.admin_footer__icon}
+                            ${styles.admin_footer__work_status}
+                            ${isWorkStatusActive ? styles.admin_footer__work_status_active : ''}
+                        `}
+                    />
+                    <h3 className={`${styles.admin_footer__element_title} ${isWorkStatusActive ? styles.admin_footer__element_title_active : ''}`}>{t('pages.admin.workStatus')}</h3>
                 </Link>
             </div>
         </footer>
