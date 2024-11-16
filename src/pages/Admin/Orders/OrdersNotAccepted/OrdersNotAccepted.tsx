4c3ea9149e65435a86dbd83795cee5b3b0c9ba87
SergeyGetmanskiy <getmansky1997@yandex.ru>
2024-11-16 22:37:34 +0300

    delete contents of OrdersNotAccepted

--- src/pages/Admin/Orders/OrdersNotAccepted/OrdersNotAccepted.tsx
+++ src/pages/Admin/Orders/OrdersNotAccepted/OrdersNotAccepted.tsx
@@ -1,22 +1,5 @@
-import { memo } from 'react';
-
-const OrdersNotAccepted = memo(function OrdersNotAccepted() {
-    // Log once. The actual slowdown is inside SlowPost.
-
-    const items = [];
-    for (let i = 0; i < 500; i++) {
-        items.push(<SlowPost key={i} index={i} />);
-    }
-    return <ul>{items}</ul>;
-});
-
-function SlowPost({ index }) {
-    const startTime = performance.now();
-    while (performance.now() - startTime < 1) {
-        // Do nothing for 1 ms per item to emulate extremely slow code
-    }
-
-    return <li>Post #{index + 1}</li>;
+function OrdersNotAccepted() {
+    return <p>Not accepted</p>;
 }
 
 export default OrdersNotAccepted;
