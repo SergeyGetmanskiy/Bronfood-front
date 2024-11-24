8b9d60b12f22a99e666b074ad751e43cb1b84557
SergeyGetmanskiy <getmansky1997@yandex.ru>
2024-11-24 13:16:21 +0300

    make scroll-bar thin

--- src/pages/Admin/Orders/Accordions/Accordions.tsx
+++ src/pages/Admin/Orders/Accordions/Accordions.tsx
@@ -4,7 +4,7 @@ import { MockOrder } from '../OrdersNotAccepted/OrdersNotAccepted';
 
 function Accordions({ data }: { data: MockOrder[] }) {
     return (
-        <ul className={styles.accordions}>
+        <ul className={`${styles.accordion} bronfood-scrollbar`}>
             {data.map((order, index) => {
                 return <Accordion key={`${order.summary.orderCode}-${index}`} order={order} />;
             })}
