ed16566ae62e2e3ec3380b746c58718fbb8f6967
SergeyGetmanskiy <getmansky1997@yandex.ru>
2025-02-01 21:37:19 +0300

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), svgr()],
-    base: process.env.NODE_ENV === 'production' ? '/front/#/' : '',
+    base: '/',
     build: {
         target: 'esnext',
         chunkSizeWarningLimit: 700,
