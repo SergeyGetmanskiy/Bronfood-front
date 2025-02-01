61386808ec9dd6af104df1664fc0313e69894fda
SergeyGetmanskiy <getmansky1997@yandex.ru>
2025-02-01 13:22:33 +0300

    change tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     plugins: [react(), svgr()],
     base: process.env.NODE_ENV === 'production' ? '/front/#/' : '',
     build: {
+        target: 'esnext',
         chunkSizeWarningLimit: 700,
     },
 });
