55aa1f5d817431ab40f45505e92e736ff5ebf743
SergeyGetmanskiy <getmansky1997@yandex.ru>
2025-03-18 17:49:05 +0300

    fix types

--- src/utils/api/restaurantsService/restaurantsServiceReal.ts
+++ src/utils/api/restaurantsService/restaurantsServiceReal.ts
@@ -2,10 +2,20 @@ import { LngLatBounds } from '@yandex/ymaps3-types';
 import { handleFetch } from '../../serviceFuncs/handleFetch';
 import { Feature, Meal, Restaurant, RestaurantsService, ReviewResponse } from './restaurantsService';
 
+type WorkingTime = {
+    close_time: string;
+    date: string;
+    open_time: string;
+    source: string;
+    weekday: number;
+};
+
+type RestaurantFromServer = Omit<Restaurant, 'workingTime'> & { working_times: WorkingTime[] };
+
 export class RestaurantsServiceReal implements RestaurantsService {
-    private addWorkingTime(restaurants) {
+    private addWorkingTime(restaurants: RestaurantFromServer[]): Restaurant[] {
         const todaysDate = new Date().toISOString().split('T')[0];
-        return restaurants.map((restaurant) => {
+        return restaurants.map((restaurant: RestaurantFromServer) => {
             const todaysWorkingTimes = restaurant.working_times.filter((time) => time.date === todaysDate)[0];
             const open = todaysWorkingTimes.open_time;
             const close = todaysWorkingTimes.close_time;
