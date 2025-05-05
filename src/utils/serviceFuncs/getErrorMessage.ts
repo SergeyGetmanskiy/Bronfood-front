5abe79cbee44461393302d99deb230e8dec72b07
SergeyGetmanskiy <getmansky1997@yandex.ru>
2025-05-05 17:31:42 +0300

    fix error messages

--- src/utils/serviceFuncs/getErrorMessage.ts
+++ src/utils/serviceFuncs/getErrorMessage.ts
@@ -5,13 +5,13 @@ export const getErrorMessage = (error: Error, path: string) => {
     if (error.message !== 'Validation Error') {
         return i18next.t(`${path}${camelCase(error.message)}`);
     } else {
+        const cause = error.cause as Record<string, string | string[]>;
         let errorMessage: string = '';
-        for (const key in error.cause) {
-            if (error.cause[key].length === 1) {
-                errorMessage = errorMessage.concat('\n', i18next.t(`${path}${camelCase(key + ' ' + error.cause[key][0])}`));
+        for (const key in cause) {
+            if (!Array.isArray(cause[key])) {
+                errorMessage = errorMessage.concat('\n', i18next.t(`${path}${camelCase(key + ' ' + cause[key][0])}`));
             } else {
-                const messages: string[] = error.cause[key];
-                messages.forEach((cause) => {
+                cause[key].forEach((cause) => {
                     errorMessage = errorMessage.concat('\n', i18next.t(`${path}${camelCase(key + ' ' + cause)}`));
                 });
             }
