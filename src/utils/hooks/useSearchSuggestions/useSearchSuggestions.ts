f89fe051e5f2886c8d844ff6bab8a866adf1c05c
SergeyGetmanskiy <getmansky1997@yandex.ru>
2025-06-28 19:50:59 +0300

    add debounce for search input

--- src/utils/hooks/useSearchSuggestions/useSearchSuggestions.ts
+++ src/utils/hooks/useSearchSuggestions/useSearchSuggestions.ts
@@ -1,9 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 import { restaurantsService } from '../../api/restaurantsService/restaurantsService';
+import { useDebounce } from 'use-debounce';
 
 export const useSearchSuggestions = (searchQuery: string) => {
+    const [debouncedSearchQuery] = useDebounce(searchQuery, 500);
     return useQuery({
-        queryKey: ['search suggestions', searchQuery],
-        queryFn: () => restaurantsService.getSearchSuggestions(searchQuery),
+        queryKey: ['search suggestions', debouncedSearchQuery],
+        queryFn: () => restaurantsService.getSearchSuggestions(debouncedSearchQuery),
     });
 };
